b300209eb19f4fa51416dfc91d1399f5d6e72ed1
melih şahin <melihsahin24@gmail.com>
2020-07-04 14:04:35 +0300

    Complete intro page

--- Login.js
+++ Login.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, Image ,View, TextInput, Button } from 'react-native';
+import { 
+    Text,
+     StyleSheet,
+      ImageBackground,
+      TouchableOpacity,
+      View,
+      Button
+     } from 'react-native';
 
 class Login extends Component {
     render() {
         return (
             <View style={ styles.container }>
-                <Image
+                <ImageBackground
                     source={require('./assets/bg.png')}
                     style={styles.backgroundImage}
                 >
-                </Image>  
+                    <Text style={styles.title}>Hoşgeldiniz</Text>
+                    <Text style={styles.description}>React Native 2. hafta ödevi</Text>
+                    
+                    <TouchableOpacity style={ styles.button}>
+                        <Text style={ styles.buttonText}>Next</Text>
+                    </TouchableOpacity>
+                </ImageBackground>  
             </View>
         )
     }
@@ -24,8 +37,37 @@ const styles = StyleSheet.create({
     },
     backgroundImage: {
         flex: 1,
-        width: null,
-        height: null,
-        resizeMode: 'cover'
+        flex: 1,
+        justifyContent: "center",
+        resizeMode: "cover",
+    },
+    title: {
+        marginTop:350,
+        marginLeft:50,
+        color: '#26233A',
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    description: {
+        marginLeft:50,
+        marginTop:15,
+        color:'#B7BDD0',
+        justifyContent:'center',
+        fontSize:18
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor:"#5D6BA4",
+        borderRadius:30,
+        marginTop:20,
+        marginRight:50,
+        marginLeft:50,
+        height:50
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 25,
+        textAlign: 'center',
+        marginVertical: 8,
     }
 });
